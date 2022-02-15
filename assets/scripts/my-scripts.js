deb44e65ea3f12bf67f76a69b9d397f2b62f520c
prajapatpraveen <pk004846@gmail.com>
2022-02-15 18:11:10 +0530

    Add Addtion function

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,1 +1,16 @@
-alert('This works');
+const defaultResult= 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
+function add() {
+    const enteredNumber=getUserNumberInput();
+    const calcDescription=`${currentResult} + ${enteredNumber}`;
+    currentResult=currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click',add);
+
