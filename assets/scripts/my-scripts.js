66162ad918638dc40d9d2e6dc5cfdbdc5cc9cb5f
prajapatpraveen <pk004846@gmail.com>
2022-02-15 18:58:52 +0530

    add functions to work button

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -24,8 +24,23 @@ function subtract() {
     createAndWriteOutput('-',initialResult,enteredNumber);
     
 }
+function multiply() {
+    const enteredNumber=getUserNumberInput();
+    const initialResult=currentResult;
+    currentResult=currentResult * enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
+    
+}
+function divide() {
+    const enteredNumber=getUserNumberInput();
+    const initialResult=currentResult;
+    currentResult=currentResult / enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
 
 
 addBtn.addEventListener('click',add);
 subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
 
