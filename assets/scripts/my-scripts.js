8b2dda4d70c9c59c63e23a69d07847a040943610
prajapatpraveen <pk004846@gmail.com>
2022-02-16 14:15:11 +0530

    use Dry object

--- assets/scripts/my-scripts.js
+++ assets/scripts/my-scripts.js
@@ -1,5 +1,6 @@
 const defaultResult= 0;
 let currentResult = defaultResult;
+let logEntry=[];
 
 function getUserNumberInput(){
     return parseInt(userInput.value);
@@ -9,33 +10,46 @@ function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
     const calcDescription=`${resultBeforeCalc} ${operator} ${calcNumber} `;
     outputResult(currentResult, calcDescription);
 }
-
+function writeToLog(operationIdentifier,prevResult,operationNumber,newResult){
+    const LogEntries={
+        operation:operationIdentifier,
+        prevResult:prevResult,
+        number:operationNumber,
+        result:newResult
+    };
+    logEntry.push(LogEntries);
+    console.log(logEntry);
+}
 function add() {
     const enteredNumber=getUserNumberInput();
     const initialResult=currentResult;
-    currentResult=currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteOutput('+',initialResult,enteredNumber);
+    writeToLog('Add',initialResult,enteredNumber,currentResult);
     
 }
 function subtract() {
     const enteredNumber=getUserNumberInput();
     const initialResult=currentResult;
-    currentResult=currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-',initialResult,enteredNumber);
+    writeToLog('Subtract',initialResult,enteredNumber,currentResult);
     
 }
 function multiply() {
     const enteredNumber=getUserNumberInput();
     const initialResult=currentResult;
-    currentResult=currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*',initialResult,enteredNumber);
+    writeToLog('Multiple',initialResult,enteredNumber,currentResult);
     
 }
 function divide() {
     const enteredNumber=getUserNumberInput();
     const initialResult=currentResult;
-    currentResult=currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/',initialResult,enteredNumber);
+    writeToLog('Divide',initialResult,enteredNumber,currentResult);
 }
 
 
